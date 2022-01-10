f8a7e2ed687f6a8c9b5d7f0882d3f20a7e254c1c
Akram ELhayani <akramelhayani@gmail.com>
2022-01-10 16:01:58 +0200

    Initial Commit

---
+++ Friend Manger Objective-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Friend Manger Objective-c
+//
+//  Created by Akram Elhayani on 10/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
